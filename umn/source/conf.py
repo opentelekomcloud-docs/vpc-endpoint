d41ab6c68767b46f7f3a2cf23cbe53d0271e825c
tischrei <tino.schreiber@t-systems.com>
2023-06-01 08:32:17 +0000

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py

--- umn/source/conf.py
+++ umn/source/conf.py
@@ -28,6 +28,15 @@ project = 'VPC Endpoint'
 otcdocs_repo_name = 'opentelekomcloud-docs/vpc-endpoint'
 # Those variables are required for edit/bug links
 
+# Those variables are needed for indexing into OpenSearch
+otcdocs_doc_environment = 'public'
+otcdocs_doc_link = '/vpc-endpoint/umn/'
+otcdocs_doc_title = 'User Guide'
+otcdocs_doc_type = 'umn'
+otcdocs_service_category = 'network'
+otcdocs_service_title = 'VPC Endpoint'
+otcdocs_service_type = 'vpcep'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
